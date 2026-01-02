f6cb390e360a7d8283e66b2e74fd0efcd014048c
0xumarkhatab <umarkhatabfrl@gmail.com>
2026-01-02 18:16:57 +0500

    nits

--- src/config.ts
+++ src/config.ts
@@ -12,10 +12,10 @@ export const SITE = {
   website: "https://0xumarkhatab.github.io/",
   author: "0xumarkhatab",
   desc: "Senior Full-stack Blockchain Engineer | 160+ Contracts Deployed | DeFi & Emerging markets Infrastructure",
-  title: "Umar's Space",
+  title: "Umar's Portfolio Site",
   postPerPage: 4,
   profile: "https://0xumarkhatab.github.io/",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "https://avatars.githubusercontent.com/u/71306738?v=4",
   lightAndDarkMode: true,
   postPerIndex: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
@@ -24,10 +24,10 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/0xumarkhatab/0xumarkhatab.github.io",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Karachi", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
